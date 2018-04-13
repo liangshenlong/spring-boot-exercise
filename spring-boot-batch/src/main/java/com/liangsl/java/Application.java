d3a4248e7bb4899fa0add9697f7eb9d9fa1c56a8
liangshenlong <314267619@qq.com>
2018-04-13 17:54:52 +0800

    增加springbatch项目

---
+++ spring-boot-batch/src/main/java/com/liangsl/java/Application.java
@@ -0,0 +1,14 @@
+package com.liangsl.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by liangsl on 2018/4/13
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args){
+        SpringApplication.run(Application.class, args);
+    }
+}
