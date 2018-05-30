374c76488f661d76feb395db54b097ca36aff9f8
liangshenlong <314267619@qq.com>
2018-05-30 10:51:01 +0800

    修改properties

--- spring-boot-web/src/main/java/com/liangsl/java/common/Properties.java
+++ spring-boot-web/src/main/java/com/liangsl/java/common/Properties.java
@@ -1,18 +1,23 @@
 package com.liangsl.java.common;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 /**
  * Created by liangsl on 2018/5/8
  */
 @Component
+@PropertySource(value = "classpath:application-mybatis.properties")
 public class Properties {
     @Value("${sys.name}")
     private String sysName;
+    @Value("${mybatis}")
+    private String mybatis;
 
     public String getSysName() {
-        return sysName;
+        return sysName+mybatis;
     }
 
     public void setSysName(String sysName) {
